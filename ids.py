3f09d1dc1202954213402e109d18b9d910d0a954
RichetinB <baptisterichetin@gmail.com>
2024-01-12 15:46:54 +0100

    test ids

--- ids.py
+++ ids.py
@@ -1,10 +1,19 @@
 import fire
 
-class Ids():
+class Ids(object):
   
 
-  def build(self):
-    print('Building IDS...')
+    def build(self):
+        f = open("/var/ids/db.json", "x")
+        
+        print('Le Build a été effectué avec succès !')
+
+    def check(self):
+        print('Checking IDS...')
+
+    def help(self):
+        print('Helping IDS...')
+
 
 if __name__ == '__main__':
   fire.Fire(Ids)
