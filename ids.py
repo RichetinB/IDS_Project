bbbc0c256a8d74019880c4231b11bb4350e0c26d
RichetinB <baptisterichetin@gmail.com>
2024-01-12 15:59:48 +0100

    test ids

--- ids.py
+++ ids.py
@@ -1,32 +1,48 @@
 import fire
 import json
-import os
 from datetime import datetime
-
 
 class Ids(object):
   
 
     @staticmethod
     def build():
-        # Initialise la structure de données à stocker dans db.json
-        data = {"build_time": str(datetime.now()), "files": {}}
+        # Appelle la fonction pour créer le fichier
+        Ids.create_db_file()
 
+        # Autres opérations liées à la construction (si nécessaire)
+        print('Le Build a été effectué avec succès !')
+
+    @staticmethod
+    def create_db_file():
         # Chemin complet pour le fichier db.json
         db_file_path = "/var/ids/db.json"
 
-        # Écrit les données dans le fichier db.json
-        with open(db_file_path, 'w') as json_file:
-            json.dump(data, json_file, indent=2)
+        try:
+            # Tente d'ouvrir le fichier en mode création ('x')
+            with open(db_file_path, 'x') as json_file:
+                # Initialise la structure de données à stocker dans db.json
+                data = {"build_time": str(datetime.now()), "files": {}}
 
-        # Enregistre dans les logs
-        Ids.log(f"Build successful. Database saved to {db_file_path}")
+                # Écrit les données dans le fichier db.json
+                json.dump(data, json_file, indent=2)
 
-    # def check(self):
-    #     print('Checking IDS...')
+                # Enregistre dans les logs
+                Ids.log(f"Build successful. Database saved to {db_file_path}")
+        except FileExistsError:
+            # Si le fichier existe déjà, imprime un message d'erreur
+            print(f"Error: {db_file_path} already exists.")
 
-    # def help(self):
-    #     print('Helping IDS...')
+    @staticmethod
+    def log(message):
+        # Implémentez la fonction de journalisation ici
+        print(message)
+
+    def check(self):
+        print('Checking IDS...')
+
+    def help(self):
+        print('Helping IDS...')
 
 
 if __name__ == '__main__':
