2f47ecb1e6f10f8837d70b62fe8ae94b3579baed
RichetinB <baptisterichetin@gmail.com>
2024-01-12 15:56:01 +0100

    test ids

--- ids.py
+++ ids.py
@@ -3,30 +3,14 @@ import json
 import os
 from datetime import datetime
 
+
 class Ids(object):
-    def __init__(self):
-        # Initialiser les attributs de la classe si nécessaire
-        pass
+  
 
     @staticmethod
     def build():
-        # Charge la configuration depuis le fichier
-        config = Ids.load_config()
-
         # Initialise la structure de données à stocker dans db.json
         data = {"build_time": str(datetime.now()), "files": {}}
-
-        # Parcours tous les fichiers et dossiers spécifiés dans la configuration
-        for path in config['files']:
-            # Si c'est un fichier, ajoute ses informations à la structure de données
-            if os.path.isfile(path):
-                data["files"][path] = Ids.get_file_info(path)
-            # Si c'est un dossier, parcourt tous les fichiers à l'intérieur
-            elif os.path.isdir(path):
-                for root, _, files in os.walk(path):
-                    for file in files:
-                        file_path = os.path.join(root, file)
-                        data["files"][file_path] = Ids.get_file_info(file_path)
 
         # Chemin complet pour le fichier db.json
         db_file_path = "/var/ids/db.json"
@@ -38,29 +22,12 @@ class Ids(object):
         # Enregistre dans les logs
         Ids.log(f"Build successful. Database saved to {db_file_path}")
 
-    @staticmethod
-    def load_config():
-        # Implémentez la fonction de chargement de la configuration ici
-        # Assurez-vous de retourner le contenu du fichier de configuration sous forme de dictionnaire
-        config_file_path = "/etc/ids/ids_config.json"
+    # def check(self):
+    #     print('Checking IDS...')
 
-        try:
-            with open(config_file_path, 'r') as json_file:
-                config = json.load(json_file)
-            return config
-        except FileNotFoundError:
-            print(f"Error: Config file not found at {config_file_path}")
-            return {}
+    # def help(self):
+    #     print('Helping IDS...')
 
-    @staticmethod
-    def get_file_info(file_path):
-        # Implémentez la fonction pour obtenir les informations d'un fichier ici
-        pass
-
-    @staticmethod
-    def log(message):
-        # Implémentez la fonction de journalisation ici
-        pass
 
 if __name__ == '__main__':
-    fire.Fire(Ids)
+  fire.Fire(Ids)
