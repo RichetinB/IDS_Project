8958784100065c9fb6ba27cba639019123069016
RichetinB <baptisterichetin@gmail.com>
2024-02-01 15:22:25 +0100

    ajout de la fonction build

--- ids.py
+++ ids.py
@@ -5,6 +5,7 @@ import __future__
 import json
 import subprocess
 from datetime import datetime
+import hashlib 
 
 # Argument 
 parser= argparse.ArgumentParser()
@@ -77,29 +78,77 @@ BaseDataConf = {
 # Function Build##############################################################################
 
 
+import hashlib
+import os
+
+# Fonction pour obtenir les informations sur un fichier
+def get_file_info(file_path):
+    file_info = {
+        "file_path": file_path,
+        "sha512": None,
+        "sha256": None,
+        "md5": None,
+        "last_modified": os.path.getmtime(file_path),
+        "creation_time": os.path.getctime(file_path),
+        "owner": os.stat(file_path).st_uid,
+        "group": os.stat(file_path).st_gid,
+        "size": os.path.getsize(file_path)
+    }
+
+    # Calcul des hash
+    with open(file_path, 'rb') as f:
+        data = f.read()
+        file_info["sha512"] = hashlib.sha512(data).hexdigest()
+        file_info["sha256"] = hashlib.sha256(data).hexdigest()
+        file_info["md5"] = hashlib.md5(data).hexdigest()
+
+    return file_info
+
+# Fonction pour obtenir les chemins des fichiers à surveiller à partir du fichier /etc/ids.json
+def get_watch_paths():
+    watch_paths = {"file": [], "dir": []}
+
+    conf_file_path = "/etc/ids.json"
+    if os.path.exists(conf_file_path):
+        with open(conf_file_path, 'r') as json_file:
+            config_data = json.load(json_file)
+            watch_paths["file"] = config_data.get("file", [])
+            watch_paths["dir"] = config_data.get("dir", [])
+
+    return watch_paths
+
+# Fonction pour construire le fichier JSON
+# Fonction pour construire le fichier JSON
 def Build():
-    if not IsInit():
+    if not is_initialized():
         print("ERREUR: Utilisez d'abord -init pour initialiser le système.")
         return
 
-    conf_file_path = "/etc/ids.json"
+    watch_paths = get_watch_paths()
+    files_info = [get_file_info(file_path) for file_path in watch_paths["file"]]
 
+    conf_file_path = "/etc/ids.json"
     with open(conf_file_path, 'r') as json_file:
         config_data = json.load(json_file)
 
     data = {
         "build_time": str(datetime.now()), 
-        "files": config_data["file"],       
-        "directories": config_data["dir"],  
+        "files": files_info,       
+        "directories": watch_paths["dir"],  
         "port": config_data["port"]          
     }
 
     db_file_path = "/var/ids/db.json"
-
     with open(db_file_path, 'w') as json_file:
-        json.dump(data, json_file,separators=(',', ':'))
+        json.dump(data, json_file, indent=4)
 
     print(f"Fichier JSON créé avec succès à l'emplacement : {db_file_path}")
+
+
+# Fonction pour vérifier si l'initialisation a déjà été effectuée
+def is_initialized() -> bool:
+    return os.path.exists("/etc/ids.json")
+
 
 
 
