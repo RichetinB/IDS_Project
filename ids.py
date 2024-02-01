355e6df24ba8109af5efcc27a98f862dde607fba
RichetinB <baptisterichetin@gmail.com>
2024-02-01 23:34:56 +0100

    ajout de la fonction build

--- ids.py
+++ ids.py
@@ -34,7 +34,7 @@ def create_init_script():
     subprocess.run(['chmod', 'u+x', script_path])
 
 def InitializeSystem():
-    subprocess.run(['/home/baptiste/IDS_Project/init_script.sh'])
+    subprocess.run(['./init_script.sh'])
 
 
 # Fonction pour construire le fichier JSON
